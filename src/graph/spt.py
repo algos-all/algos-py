20f4fd2778567f9f24fadcb6e7a4010b873db837
Alexander Lisianoi <all3fox@gmail.com>
2016-02-25 22:04:06 +0300

    Implement Bellman-Ford, cycle detection looks way to bulky.

--- src/graph/spt.py
+++ src/graph/spt.py
@@ -1,7 +1,8 @@
 from heap import Heap
+from collections import deque
 
 
-class LazyDijkstra:
+class Dijkstra:
     def __init__(self, wdgraph, src=None):
         if src not in wdgraph.edges:
             raise RuntimeError("Source node not in graph")
@@ -28,3 +29,76 @@ class LazyDijkstra:
 
             for n3, w in wdgraph.edges[n2]:
                 edge_costs.push([n2, n3, w, c + w])
+
+
+class BellmanFord:
+    def __init__(self, wdgraph, src=None):
+        if src not in wdgraph.edges:
+            raise RuntimeError("Source node not in graph")
+
+        self.costs = {node : None for node in wdgraph.edges}
+        self.paths = {node : None for node in wdgraph.edges}
+
+        this_epoch = deque(maxlen=len(wdgraph.edges))
+        next_epoch = {node : False for node in wdgraph.edges}
+
+        self.costs[src] = 0
+        self.paths[src] = None
+
+        this_epoch.append(src)
+        next_epoch[src] = True
+
+        i, j = 0, 1
+
+        while this_epoch:
+            n1 = this_epoch.popleft()
+            next_epoch[n1] = False
+
+            cost1 = self.costs[n1]
+            for n2, w in wdgraph.edges[n1]:
+                cost2 = self.costs[n2]
+
+                if cost2 is not None and cost2 < cost1 + w: continue
+
+                self.costs[n2] = cost1 + w
+                self.paths[n2] = n1
+
+                if not next_epoch[n2]:
+                    this_epoch.append(n2)
+                    next_epoch[n2] = True
+
+            j -= 1
+
+            if j == 0: i, j = i + 1, len(this_epoch)
+
+            if i % (len(wdgraph.edges) + 1) == 0:
+                break
+
+        self._check_negative_cycle()
+
+    def _check_negative_cycle(self):
+        onpath = {n : None for n in self.paths}
+
+        for i, n in enumerate(self.paths):
+            if onpath[n] is not None:
+                continue
+
+            while n is not None and onpath[n] is None:
+                onpath[n] = i
+
+                n = self.paths[n]
+
+            if n is not None and onpath[n] == i: break
+        else:
+            self.cycle = []
+            return
+
+        self.cycle = [n]
+        n = self.paths[n]
+
+        while self.cycle[0] is not n:
+            self.cycle.append(n)
+
+            n = self.paths[n]
+
+        self.cycle = list(reversed(self.cycle))
--- tests/test_graph/test_spt.py
+++ tests/test_graph/test_spt.py
@@ -3,7 +3,7 @@ import math, random
 from nose.tools import raises
 
 from graph.wgraph import WeightedDiGraph
-from graph.spt import LazyDijkstra
+from graph.spt import Dijkstra, BellmanFord
 
 
 class CheckShortestPath:
@@ -16,6 +16,22 @@ class CheckShortestPath:
                     return False
 
         return True
+
+    def is_negative_cycle(self, spt, wdgraph):
+        assert len(spt.cycle) > 0, spt.cycle
+
+        total = 0
+        for i in range(1, len(spt.cycle)):
+            n1, n2 = spt.cycle[i - 1], spt.cycle[i]
+
+            total += min([w for n, w in wdgraph.edges[n1] if n == n2])
+
+        n1, n2 = spt.cycle[-1], spt.cycle[0]
+        total += min([w for n, w in wdgraph.edges[n1] if n == n2])
+
+        if total < 0: return True
+
+        return False
 
     @raises(RuntimeError)
     def check_empty(self, spt):
@@ -46,20 +62,140 @@ class CheckShortestPath:
         h = spt(g, n1)
 
         assert self.is_shortest(h, g)
+        if isinstance(h, BellmanFord):
+            assert h.cycle == [], h.cycle
+
+    def check_random_negative(self, spt, V=10, E=10, seed=0):
+        assert V > 1 and E > 0
+        random.seed(seed)
+
+        g = WeightedDiGraph()
+
+        for i in range(E):
+            n1, n2 = random.sample(range(V), 2)
+            g.add_edge(n1, n2, random.random() - 0.5)
+
+        h = spt(g, n1)
+
+        if h.cycle != []:
+            assert self.is_negative_cycle(h, g), h.cycle
+        else:
+            assert self.is_shortest(h, g)
 
 
-class TestShortestPath(CheckShortestPath):
+class TestDijkstra(CheckShortestPath):
     def test_empty(self):
-        yield self.check_empty, LazyDijkstra
+        yield self.check_empty, Dijkstra
 
     def test_chain(self, ns=range(1, 10)):
         for n in ns:
-            yield self.check_chain, LazyDijkstra, n
+            yield self.check_chain, Dijkstra, n
 
-    def test_random_0(self, ns=range(2, 8)):
+    def test_random_0(self, ns=range(2, 8), ntimes=10):
         for n in ns:
-            yield self.check_random, LazyDijkstra, n, math.factorial(n)
+            e = math.factorial(n)
 
-    def test_random_1(self, ns=range(2, 100)):
+            for i in range(ntimes):
+                yield self.check_random, Dijkstra, n, e, i
+
+    def test_random_1(self, ns=range(2, 100), ntimes=10):
         for n in ns:
-            yield self.check_random, LazyDijkstra, n, n
+            for i in range(ntimes):
+                yield self.check_random, Dijkstra, n, n
+
+
+class TestBellmanFord(CheckShortestPath):
+    def test_empty(self):
+        yield self.check_empty, BellmanFord
+
+    def test_chain(self, ns=range(1, 10)):
+        for n in ns:
+            yield self.check_chain, BellmanFord, n
+
+    def test_random_0(self, ns=range(2, 8), ntimes=10):
+        for n in ns:
+            e = math.factorial(n)
+
+            for i in range(ntimes):
+                yield self.check_random, BellmanFord, n, e, i
+
+    def test_random_1(self, ns=range(2, 100), ntimes=10):
+        for n in ns:
+            for i in range(ntimes):
+                yield self.check_random, BellmanFord, n, n, i
+
+    def test_negative_0(self):
+        g = WeightedDiGraph()
+        g.add_edge(0, 1, -1)
+
+        h = BellmanFord(g, 0)
+
+        assert h.costs[0] == 0
+        assert h.costs[1] == -1
+        assert h.paths[0] == None
+        assert h.paths[1] == 0
+
+    def test_negative_1(self):
+        g = WeightedDiGraph()
+        g.add_edge(0, 1, -1)
+        g.add_edge(0, 1, -2)
+
+        h = BellmanFord(g, 0)
+
+        assert h.costs[0] == 0
+        assert h.costs[1] == -2
+        assert h.paths[0] == None
+        assert h.paths[1] == 0
+
+    def test_negative_cycle_0(self):
+        g = WeightedDiGraph()
+        g.add_edge(0, 1, -1)
+        g.add_edge(1, 0, -1)
+
+        h = BellmanFord(g, 0)
+
+        assert self.is_negative_cycle(h, g)
+
+    def test_negative_cycle_1(self):
+        g = WeightedDiGraph()
+        g.add_edge(0, 0, -1)
+
+        h = BellmanFord(g, 0)
+
+        assert self.is_negative_cycle(h, g)
+
+    def test_negative_cycle_2(self):
+        g = WeightedDiGraph()
+
+        g.add_edge(0, 1, 1)
+        g.add_edge(1, 2, 1)
+        g.add_edge(2, 0, -3)
+
+        h = BellmanFord(g, 0)
+
+        assert self.is_negative_cycle(h, g), h.cycle
+
+    def test_disconnected_0(self):
+        g = WeightedDiGraph()
+
+        g.edges[0] = []
+        g.edges[1] = []
+
+        h = BellmanFord(g, 0)
+
+        assert h.costs[0] == 0
+        assert h.costs[1] == None
+        assert h.paths[0] == None
+        assert h.paths[1] == None
+
+    def test_random_negative_1(self, ns=range(2, 8), ntimes=100):
+        for n in ns:
+            e = math.factorial(n)
+
+            for i in range(ntimes):
+                yield self.check_random_negative, BellmanFord, n, e, i
+
+    def test_random_negative_1(self, ns=range(2, 100), ntimes=100):
+        for n in ns:
+            for i in range(ntimes):
+                yield self.check_random_negative, BellmanFord, n, n, i
