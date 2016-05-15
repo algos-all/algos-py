8f8bda0f9562adcf002713c43b2bedc4cb6e6a1f
Alexander Lisianoi <all3fox@gmail.com>
2016-05-15 23:10:22 +0200

    Heapsort: use stricter break condition

--- src/sort/heapsort.py
+++ src/sort/heapsort.py
@@ -1,22 +1,22 @@
-def heapsort(l):
-    def plunge(l, i, n):
+def heapsort(xs):
+    def plunge(xs, i, n):
         lchild = 2 * i + 1
 
         while lchild < n:
             rchild = lchild + 1
-            if rchild < n and l[rchild] > l[lchild]:
+            if rchild < n and xs[lchild] < xs[rchild]:
                 child = rchild
             else:
                 child = lchild
 
-            if l[i] > l[child]: break
+            if xs[i] >= xs[child]: break
 
-            l[i], l[child] = l[child], l[i]
+            xs[i], xs[child] = xs[child], xs[i]
             i, lchild = child, 2 * child + 1
 
-    for i in range(len(l) // 2, -1, -1):
-        plunge(l, i, len(l))
+    for i in range(len(xs) // 2, -1, -1):
+        plunge(xs, i, len(xs))
 
-    for i in range(len(l) - 1, -1, -1):
-        l[0], l[i] = l[i], l[0]
-        plunge(l, 0, i)
+    for i in range(len(xs) - 1, -1, -1):
+        xs[0], xs[i] = xs[i], xs[0]
+        plunge(xs, 0, i)
