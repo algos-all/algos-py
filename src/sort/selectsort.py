363f684aa395799922ab7e86905aa298ccd7394c
Alexander Lisianoi <all3fox@gmail.com>
2016-09-14 20:23:28 +0200

    Selectsort: use fewer element exchanges, speed up by about 40%

--- src/sort/selectsort.py
+++ src/sort/selectsort.py
@@ -2,15 +2,18 @@ import operator
 
 
 def selectsort0(xs, key=lambda x: x, reverse=False):
-    n = len(xs)
-    ys = [key(x) for x in xs]
-    cmp = operator.gt if reverse else operator.lt
+    n, ys = len(xs), [key(x) for x in xs]
+    cmp = operator.ge if reverse else operator.le
 
     for i in range(n - 1):
+        a, k = ys[i], i
         for j in range(i + 1, n):
-            if cmp(ys[j], ys[i]):
-                xs[i], xs[j] = xs[j], xs[i]
-                ys[i], ys[j] = ys[j], ys[i]
+            a, k = (a, k) if cmp(a, ys[j]) else (ys[j], j)
+
+        if not cmp(ys[i], ys[k]):
+            ys[i], ys[k] = ys[k], ys[i]
+            xs[i], xs[k] = xs[k], xs[i]
+
 
 def selectsort1(xs, key=lambda x: x, reverse=False):
     ys = [key(x) for x in xs]
