609f759c12376b8436038d97801f073d0b7cb256
Alexander Lisianoi <all3fox@gmail.com>
2016-09-17 23:41:50 +0200

    Selectsort: simplify inner loop

--- src/sort/selectsort.py
+++ src/sort/selectsort.py
@@ -17,12 +17,10 @@ def selectsort0(xs, key=lambda x: x, reverse=False):
 
 def selectsort1(xs, key=lambda x: x, reverse=False):
     ys = [key(x) for x in xs]
-    nxt = max if reverse else min
-    cmp = operator.gt if reverse else operator.lt
+    cmp = max if reverse else min
 
     for i in range(len(xs) - 1):
-        j = i + 1 + ys[i + 1:].index(nxt(ys[i + 1:]))
+        j = i + ys[i:].index(cmp(ys[i:]))
 
-        if cmp(ys[j], ys[i]):
-            xs[i], xs[j] = xs[j], xs[i]
-            ys[i], ys[j] = ys[j], ys[i]
+        xs[j], xs[i] = xs[i], xs[j]
+        ys[j], ys[i] = ys[i], ys[j]
