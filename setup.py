9f81fb49307ba85795f7e22b9693239bc6b32131
Alexander Lisianoi <all3fox@gmail.com>
2017-02-28 19:12:29 +0100

    Add setup.py, __init__.py in all packages, and MANIFEST.in
    [skip ci]

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='algos-py',
+    version='0.1.0',
+    license='MIT',
+    author='Aleksandr Lisianoi',
+    author_email='all3fox@gmail.com',
+    url='https://github.com/all3fox/algos-py',
+    packages=[
+        'src', 'src.sort', 'src.graph', 'src.sset',
+        'tests', 'tests.test_graph', 'tests.test_sort', 'tests.test_sset'
+    ],
+    description="Classic computer science algorithms in Python",
+    long_description="Classic computer science algorithms in Python",
+    platforms=['linux']
+)
