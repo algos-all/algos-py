281a4722eb62ebd4158660bf9add0ac5da7d2d3b
Alexander Lisianoi <all3fox@gmail.com>
2017-03-22 23:38:04 +0100

    Reimplement heapsort

--- src/sort/heapsort.py
+++ src/sort/heapsort.py
@@ -1,31 +1,63 @@
 import operator
 
 
-def heapsort(xs, key=lambda x: x, reverse=False):
-    def plunge(i, n):
-        lchild = 2 * i + 1
-
-        while lchild < n:
-            rchild = lchild + 1
-            if rchild < n and cmp(ys[lchild], ys[rchild]):
-                child = rchild
-            else:
-                child = lchild
-
-            if cmp(ys[child], ys[i]): break
-
-            xs[i], xs[child] = xs[child], xs[i]
-            ys[i], ys[child] = ys[child], ys[i]
-            i, lchild = child, 2 * child + 1
-
-    n = len(xs)
-    ys = [key(x) for x in xs]
+def sink(xs, ys, i, n, reverse=False):
     cmp = operator.gt if reverse else operator.lt
 
-    for i in range(n // 2, -1, -1):
-        plunge(i, n)
+    lchild = 2 * i + 1
 
-    for i in range(n - 1, -1, -1):
+    while lchild < n:
+        rchild = lchild + 1
+
+        if rchild < n and cmp(ys[rchild], ys[lchild]):
+            child = rchild
+        else:
+            child = lchild
+
+        if not cmp(ys[child], ys[i]):
+            break
+
+        xs[child], xs[i] = xs[i], xs[child]
+        ys[child], ys[i] = ys[i], ys[child]
+
+        i, lchild = child, 2 * child + 1
+
+
+def swim(xs, ys, i, reverse=False):
+    cmp = operator.gt if reverse else operator.lt
+
+    while i != 0:
+        parent = (i - 1) // 2
+
+        if not cmp(ys[i], ys[parent]):
+            return
+
+        xs[parent], xs[i] = xs[i], xs[parent]
+        ys[parent], ys[i] = ys[i], ys[parent]
+
+        i = parent
+
+
+def xheapify(xs, ys, reverse=False):
+    for i in range(len(xs) // 2, -1, -1):
+        sink(xs, ys, i, len(xs), reverse=reverse)
+
+
+def heapify(xs, key=lambda x: x, reverse=False):
+    xheapify(xs, [key(x) for x in xs], reverse=reverse)
+
+
+def xheapsort(xs, ys, reverse=False):
+    for i in range(len(xs) - 1, -1, -1):
         xs[0], xs[i] = xs[i], xs[0]
         ys[0], ys[i] = ys[i], ys[0]
-        plunge(0, i)
+
+        sink(xs, ys, 0, i, not reverse)
+
+
+def heapsort(xs, key=lambda x: x, reverse=False):
+    ys = [key(x) for x in xs]
+
+    xheapify(xs, ys, not reverse)
+
+    xheapsort(xs, ys, reverse=reverse)
