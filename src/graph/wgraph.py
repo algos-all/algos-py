6cb228238e296041063460d4b3d15e1f5065f0e0
Alexander Lisianoi <all3fox@gmail.com>
2017-03-22 23:33:56 +0100

    Fix imports in graph-related modules (sources and tests)

--- src/graph/wgraph.py
+++ src/graph/wgraph.py
@@ -1,4 +1,4 @@
-from src.graph.agraph import AbstractGraph
+from graph.agraph import AbstractGraph
 
 
 class WeightedGraph(AbstractGraph):
--- tests/test_graph/test_graph.py
+++ tests/test_graph/test_graph.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-from src.graph.graph import Graph, DiGraph
+from graph.graph import Graph, DiGraph
 
 
 class TestGraph:
