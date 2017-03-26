070caf43c1eb10d2735ef4a80aeea2e7366a16a5
Alexander Lisianoi <all3fox@gmail.com>
2017-03-26 11:27:38 +0200

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as readme:
 
 setup(
     name='algos-py',
-    version='0.3.1',
+    version='0.3.2',
     license='MIT',
     author='Aleksandr Lisianoi',
     author_email='all3fox@gmail.com',
