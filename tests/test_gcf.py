f6d484d74d6f85549365463af31ab627c1486f27
Alexander Lisianoi <all3fox@gmail.com>
2017-05-07 12:21:16 +0200

    Pytest: use @pytest.mark.parametrize in test_gcf

--- tests/test_gcf.py
+++ tests/test_gcf.py
@@ -1,53 +1,46 @@
-import random
+import random, pytest
 
 from fractions import gcd
 from src.gcf import gcf, xgcf
 
 
-class TestGCF:
-    def test_00(self):
-        assert gcf(0, 0) == 0
+def test_00():
+    assert gcf(0, 0) == 0
 
-    def test_10(self):
-        assert gcf(1, 0) == 1
+def test_10():
+    assert gcf(1, 0) == 1
 
-    def test_01(self):
-        assert gcf(0, 1) == 1
+def test_01():
+    assert gcf(0, 1) == 1
 
-    def check_random(self, lo=-1024, hi=1024, seed=0):
-        random.seed(seed)
+@pytest.mark.parametrize("seed", list(range(100)))
+def test_random_gcd(seed, lo=-1024, hi=1024):
+    random.seed(seed)
 
-        a, b = random.randint(lo, hi), random.randint(lo, hi)
+    a, b = random.randint(lo, hi), random.randint(lo, hi)
 
-        g = gcf(a, b)
+    g = gcf(a, b)
 
-        assert g == gcd(a, b), "{}, {}".format(a, b)
+    assert g == gcd(a, b), "{}, {}".format(a, b)
 
-    def test_random(self, times=10000):
-        for i in range(times): yield self.check_random
+def test_00():
+    assert xgcf(0, 0)[0] == 0
 
+def test_10():
+    assert xgcf(1, 0) == (1, 1, 0)
 
-class TestXGCF:
-    def test_00(self):
-        assert xgcf(0, 0)[0] == 0
+def test_01():
+    assert xgcf(0, 1) == (1, 0, 1)
 
-    def test_10(self):
-        assert xgcf(1, 0) == (1, 1, 0)
+@pytest.mark.parametrize("seed", list(range(100)))
+def test_random_gcf(seed, lo=-1024, hi=1024):
+    random.seed(seed)
 
-    def test_01(self):
-        assert xgcf(0, 1) == (1, 0, 1)
+    a, b = random.randint(lo, hi), random.randint(lo, hi)
 
-    def check_random(self, lo=-1024, hi=1024, seed=0):
-        random.seed(seed)
+    g, s, t = xgcf(a, b)
+    correct = gcd(a, b)
 
-        a, b = random.randint(lo, hi), random.randint(lo, hi)
-
-        g, s, t = xgcf(a, b)
-        correct = gcd(a, b)
-
-        assert correct == g, "{}, {}".format(a, b)
-        assert correct == s * a + t * b, "{}, {}".format(a, b)
-
-    def test_random(self, times=10000):
-        for i in range(times): yield self.check_random
+    assert correct == g, "{}, {}".format(a, b)
+    assert correct == s * a + t * b, "{}, {}".format(a, b)
 
