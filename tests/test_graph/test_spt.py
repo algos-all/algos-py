85bf4d4c0656df9c0c3b5d118316920f64add02d
Alexander Lisianoi <all3fox@gmail.com>
2017-05-07 09:57:02 +0200

    Pytest: switch test_graph/test_spt.py to @pytest.mark.parametrize

--- tests/test_graph/test_spt.py
+++ tests/test_graph/test_spt.py
@@ -6,226 +6,189 @@ from src.graph.wgraph import WeightedDiGraph
 from src.graph.spt import Dijkstra, BellmanFord
 
 
-class CheckShortestPath:
-    def is_shortest(self, spt, wdgraph):
-        for n1 in wdgraph:
-            # there may be several connected components
-            if spt.costs[n1] is None: continue
+def is_shortest(spt, wdgraph):
+    for n1 in wdgraph:
+        # there may be several connected components
+        if spt.costs[n1] is None: continue
 
-            for n2, w in wdgraph[n1]:
-                if spt.costs[n2] > spt.costs[n1] + w:
-                    return False
+        for n2, w in wdgraph[n1]:
+            if spt.costs[n2] > spt.costs[n1] + w:
+                return False
 
+    return True
+
+def is_negative_cycle(spt, wdgraph):
+    def min_edge(n1, n2):
+        ws = [w for n, w in wdgraph[n1] if n == n2]
+
+        assert ws, "n1 and n2 are not actually connected"
+
+        return min(ws)
+
+    assert len(spt.cycle) > 0, spt.cycle
+
+    total = 0
+    for i in range(1, len(spt.cycle)):
+        total += min_edge(spt.cycle[i - 1], spt.cycle[i])
+    total += min_edge(spt.cycle[-1], spt.cycle[0])
+
+    if total < 0:
         return True
 
-    def is_negative_cycle(self, spt, wdgraph):
-        def min_edge(n1, n2):
-            ws = [w for n, w in wdgraph[n1] if n == n2]
-            assert ws, "n1 and n2 are not actually connected"
+    return False
 
-            return min(ws)
+@pytest.mark.parametrize("SPT", [Dijkstra, BellmanFord])
+def test_empty(SPT):
+    with pytest.raises(RuntimeError):
+        SPT(WeightedDiGraph())
 
-        assert len(spt.cycle) > 0, spt.cycle
+@pytest.mark.parametrize("SPT", [Dijkstra, BellmanFord])
+def test_source(SPT):
+    g = WeightedDiGraph()
 
-        total = 0
-        for i in range(1, len(spt.cycle)):
-            total += min_edge(spt.cycle[i - 1], spt.cycle[i])
-        total += min_edge(spt.cycle[-1], spt.cycle[0])
+    g.add_edge(0, 1, 2)
 
-        if total < 0:
-            return True
+    h = SPT(g, 0)
 
-        return False
+    assert h.costs[0] == 0, h.costs[0]
+    assert h.costs[1] == 2, h.costs[1]
 
-    def check_empty(self, spt):
-        with pytest.raises(RuntimeError):
-            spt(WeightedDiGraph())
+@pytest.mark.parametrize("SPT", [Dijkstra, BellmanFord])
+@pytest.mark.parametrize("N", list(range(1, 10)))
+def test_chain(SPT, N):
+    assert N > 0
 
-    def check_source(self, spt):
-        g = WeightedDiGraph()
+    g = WeightedDiGraph()
 
-        g.add_edge(0, 1, 2)
+    for i in range(N):
+        g.add_edge(i, i + 1, 1)
 
-        h = spt(g, 0)
+    h = SPT(g, 0)
 
-        assert h.costs[0] == 0, h.costs[0]
-        assert h.costs[1] == 2, h.costs[1]
+    assert is_shortest(h, g)
 
-    def check_chain(self, spt, N=10):
-        assert N > 0
+@pytest.mark.parametrize("SPT", [Dijkstra, BellmanFord])
+@pytest.mark.parametrize(
+    "V,E", [(i, i ** 2) for i in range(2, 50)]
+)
+@pytest.mark.parametrize("seed", list(range(3)))
+def test_random(SPT, V, E, seed):
+    assert V > 1 and E > 0
 
-        g = WeightedDiGraph()
+    random.seed(seed)
 
-        for i in range(N):
-            g.add_edge(i, i + 1, 1)
+    g = WeightedDiGraph()
 
-        h = spt(g, 0)
+    for i in range(E):
+        n1, n2 = random.sample(range(V), 2)
+        g.add_edge(n1, n2, random.random())
 
-        assert self.is_shortest(h, g)
+    h = SPT(g, n1)
 
-    def check_random(self, spt, V=10, E=10, seed=0):
-        assert V > 1 and E > 0
-        random.seed(seed)
+    assert is_shortest(h, g)
+    
+    if isinstance(h, BellmanFord):
+        assert h.cycle == [], h.cycle
 
-        g = WeightedDiGraph()
+@pytest.mark.parametrize("SPT", [BellmanFord])
+@pytest.mark.parametrize(
+    "V,E", [(i, i ** 2) for i in range(2, 50)]
+)
+@pytest.mark.parametrize("seed", list(range(3)))
+def test_random_negative(SPT, V, E, seed):
+    assert V > 1 and E > 0
 
-        for i in range(E):
-            n1, n2 = random.sample(range(V), 2)
-            g.add_edge(n1, n2, random.random())
+    random.seed(seed)
 
-        h = spt(g, n1)
+    g = WeightedDiGraph()
+    j = WeightedDiGraph()
 
-        assert self.is_shortest(h, g)
-        if isinstance(h, BellmanFord):
-            assert h.cycle == [], h.cycle
+    for i in range(E):
+        n1, n2 = random.sample(range(V), 2)
+        g.add_edge(n1, n2, random.random() - 0.5)
 
-    def check_random_negative(self, spt, V=10, E=10, seed=0):
-        assert V > 1 and E > 0
-        random.seed(seed)
+    for src in g:
+        for dst, w in g[src]:
+            j.add_edge(src, dst, w)
 
-        g = WeightedDiGraph()
-        j = WeightedDiGraph()
+    h = SPT(g, n1)
 
-        for i in range(E):
-            n1, n2 = random.sample(range(V), 2)
-            g.add_edge(n1, n2, random.random() - 0.5)
+    for src in j:
+        for dst, w in j[src]:
+            assert [dst, w] in g[src]
 
-        for src in g:
-            for dst, w in g[src]:
-                j.add_edge(src, dst, w)
+    if h.cycle:
+        assert is_negative_cycle(h, g), h.cycle
+    else:
+        assert is_shortest(h, g)
 
-        h = spt(g, n1)
+def test_negative_0():
+    g = WeightedDiGraph()
 
-        for src in j:
-            for dst, w in j[src]:
-                assert [dst, w] in g[src]
+    g.add_edge(0, 1, -1)
 
-        if h.cycle:
-            assert self.is_negative_cycle(h, g), h.cycle
-        else:
-            assert self.is_shortest(h, g)
+    h = BellmanFord(g, 0)
 
+    assert h.costs[0] == 0
+    assert h.costs[1] == -1
+    assert h.paths[0] == None
+    assert h.paths[1] == 0
 
-class TestDijkstra(CheckShortestPath):
-    def test_empty(self):
-        yield self.check_empty, Dijkstra
+def test_negative_1():
+    g = WeightedDiGraph()
 
-    def test_source(self):
-        yield self.check_source, Dijkstra
+    g.add_edge(0, 1, -1)
+    g.add_edge(0, 1, -2)
 
-    def test_chain(self, ns=range(1, 10)):
-        for n in ns:
-            yield self.check_chain, Dijkstra, n
+    h = BellmanFord(g, 0)
 
-    def test_random_0(self, ns=range(2, 8), ntimes=10):
-        for n in ns:
-            e = math.factorial(n)
+    assert h.costs[0] == 0
+    assert h.costs[1] == -2
+    assert h.paths[0] == None
+    assert h.paths[1] == 0
 
-            for i in range(ntimes):
-                yield self.check_random, Dijkstra, n, e, i
+def test_negative_cycle_0():
+    g = WeightedDiGraph()
 
-    def test_random_1(self, ns=range(2, 100), ntimes=10):
-        for n in ns:
-            for i in range(ntimes):
-                yield self.check_random, Dijkstra, n, n
+    g.add_edge(0, 1, -1)
+    g.add_edge(1, 0, -1)
 
+    h = BellmanFord(g, 0)
 
-class TestBellmanFord(CheckShortestPath):
-    def test_empty(self):
-        yield self.check_empty, BellmanFord
+    print(h.cycle)
+    print(h.paths)
 
-    def test_source(self):
-        yield self.check_source, BellmanFord
+    assert is_negative_cycle(h, g)
 
-    def test_chain(self, ns=range(1, 10)):
-        for n in ns:
-            yield self.check_chain, BellmanFord, n
+def test_negative_cycle_1():
+    g = WeightedDiGraph()
 
-    def test_random_0(self, ns=range(2, 8), ntimes=10):
-        for n in ns:
-            e = math.factorial(n)
+    g.add_edge(0, 0, -1)
 
-            for i in range(ntimes):
-                yield self.check_random, BellmanFord, n, e, i
+    h = BellmanFord(g, 0)
 
-    def test_random_1(self, ns=range(2, 100), ntimes=10):
-        for n in ns:
-            for i in range(ntimes):
-                yield self.check_random, BellmanFord, n, n, i
+    assert is_negative_cycle(h, g)
 
-    def test_negative_0(self):
-        g = WeightedDiGraph()
-        g.add_edge(0, 1, -1)
+def test_negative_cycle_2():
+    g = WeightedDiGraph()
 
-        h = BellmanFord(g, 0)
+    g.add_edge(0, 1, 1)
+    g.add_edge(1, 2, 1)
+    g.add_edge(2, 0, -3)
 
-        assert h.costs[0] == 0
-        assert h.costs[1] == -1
-        assert h.paths[0] == None
-        assert h.paths[1] == 0
+    h = BellmanFord(g, 0)
 
-    def test_negative_1(self):
-        g = WeightedDiGraph()
-        g.add_edge(0, 1, -1)
-        g.add_edge(0, 1, -2)
+    assert is_negative_cycle(h, g), h.cycle
 
-        h = BellmanFord(g, 0)
+def test_disconnected_0():
+    g = WeightedDiGraph()
 
-        assert h.costs[0] == 0
-        assert h.costs[1] == -2
-        assert h.paths[0] == None
-        assert h.paths[1] == 0
+    g.graph[0] = []
+    g.graph[1] = []
 
-    def test_negative_cycle_0(self):
-        g = WeightedDiGraph()
-        g.add_edge(0, 1, -1)
-        g.add_edge(1, 0, -1)
+    h = BellmanFord(g, 0)
 
-        h = BellmanFord(g, 0)
-
-        print(h.cycle)
-        print(h.paths)
-
-        assert self.is_negative_cycle(h, g)
-
-    def test_negative_cycle_1(self):
-        g = WeightedDiGraph()
-        g.add_edge(0, 0, -1)
-
-        h = BellmanFord(g, 0)
-
-        assert self.is_negative_cycle(h, g)
-
-    def test_negative_cycle_2(self):
-        g = WeightedDiGraph()
-
-        g.add_edge(0, 1, 1)
-        g.add_edge(1, 2, 1)
-        g.add_edge(2, 0, -3)
-
-        h = BellmanFord(g, 0)
-
-        assert self.is_negative_cycle(h, g), h.cycle
-
-    def test_disconnected_0(self):
-        g = WeightedDiGraph()
-
-        g.graph[0] = []
-        g.graph[1] = []
-
-        h = BellmanFord(g, 0)
-
-        assert h.costs[0] == 0
-        assert h.costs[1] == None
-        assert h.paths[0] == None
-        assert h.paths[1] == None
-
-    def test_random_negative_1(self, ns=range(2, 8), ntimes=100):
-        for n, i in product(ns, range(ntimes)):
-            e = math.factorial(n)
-
-            yield self.check_random_negative, BellmanFord, n, e, i
-
-    def test_random_negative_1(self, ns=range(2, 100), ntimes=100):
-        for n, i in product(ns, range(ntimes)):
-            yield self.check_random_negative, BellmanFord, n, n, i
+    assert h.costs[0] == 0
+    assert h.costs[1] == None
+    assert h.paths[0] == None
+    assert h.paths[1] == None
