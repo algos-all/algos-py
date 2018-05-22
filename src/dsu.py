66b98d8e70d4f8e5b4d36c0fa6052ac4e65cc585
Alexander Lisianoi <all3fox@gmail.com>
2018-05-22 20:33:30 +0200

    Add comments to DisjointSetUnion

--- src/dsu.py
+++ src/dsu.py
@@ -1,6 +1,29 @@
 class DisjointSetUnion:
+    '''
+    Construct a disjoint set union (DSU)
+
+    This data structure tracks a set of elements partitioned into a
+    number of *disjoint* (i.e. non-overlapping) subsets. It provides
+    almost constant-time operations to:
+
+    1. Add a new element to the set
+    2. Join two sets into one (union)
+    3. Find if two elements are in the same set (find)
+
+    Alternative names for this data structure:
+
+    1. Disjoint-set data structure
+    2. Union-find data structure
+    3. Merge-find set
+    '''
 
     def __init__(self, xs=None):
+        '''
+        Create an empty or a pre-populated disjoint set union (DSU)
+
+        :param xs: a list of elements. If None, then an empty DSU is
+        created. Otherwise, each element is treated as its own set.
+        '''
         self.xs = {x: x for x in xs} if xs else {}
         self.ws = {x: 1 for x in xs} if xs else {}
 
@@ -13,8 +36,17 @@ class DisjointSetUnion:
         return self.find(key)
 
     def __setitem__(self, key, val):
+        '''
+        Add a new element to the disjoint set union (DSU)
+
+        The item is added initially as its own set.
+
+        :param key: the item to add to the DSU
+        :param val: the value, must be equal to the provided key
+        '''
+
         if key is not val:
-            raise RuntimeError("key and val must be the same object")
+            raise RuntimeError("key and val must be the same")
 
         self.xs[key] = key
         self.ws[key] = 1
