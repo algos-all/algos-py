c128994c7e46406bba9ae0ddf291cde4ab94611e
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-11-29 14:28:42 +0100

    Fix countsort: use fewer new lists. Add docstring

--- src/sort/countsort.py
+++ src/sort/countsort.py
@@ -1,25 +1,45 @@
-def countsort(xs, key=lambda x: x, reverse=False):
-    if len(xs) <= 1:
+def countsort(xs, key = lambda x: x, reverse = False):
+    """
+    Performs counting sort on the provided list
+
+    Args:
+        xs (List[T]): a list of objects to sort
+        key (Callable([T], Q)): a function to produce a comparable object
+        reverse (bool): True if the sorting order should be reversed, False by default
+
+    Returns:
+        The reference to the new list. The new list is sorted.
+    """
+
+    N = len(xs)
+
+    if N == 0 or N == 1:
         return xs
 
-    ys = list(range(len(xs)))
-    zs = list(zip([key(x) for x in xs], xs))
+    ys, zs = [key(x) for x in xs], list(range(N))
 
-    mi, ma = min(zs)[0], max(zs)[0]
-    cs = [0 for i in range(0, ma - mi + 2)]
+    lower, upper = min(ys), max(ys)
+    cs = [0 for i in range(0, upper - lower + 2)]
 
-    for z in zs: cs[z[0] - mi + 1] += 1
+    for y in ys:
+        cs[y - lower + 1] += 1
 
     for i in range(1, len(cs)):
         cs[i] += cs[i - 1]
 
     if reverse:
-        for z in zs:
-            ys[len(ys) - 1 - cs[z[0] - mi]] = z[1]
-            cs[z[0] - mi] += 1
-    else:
-        for z in zs:
-            ys[cs[z[0] - mi]] = z[1]
-            cs[z[0] - mi] += 1
+        for i in range(N):
+            ci = ys[i] - lower
 
-    return ys
+            zs[N - 1 - cs[ci]] = xs[i]
+
+            cs[ci] += 1
+    else:
+        for i in range(N):
+            ci = ys[i] - lower
+
+            zs[cs[ci]] = xs[i]
+
+            cs[ci] += 1
+
+    return zs
